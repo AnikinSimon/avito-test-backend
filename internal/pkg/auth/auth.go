4ede6d4f9142afe3af78af455f706a00ee8a5004
AnikinSimon <s.anikin1309@gmail.com>
2025-04-20 18:35:02 +0300

    Add auth and jwt implementations

--- internal/pkg/auth/auth.go
+++ internal/pkg/auth/auth.go
@@ -1,1 +1,94 @@
 package auth
+
+import (
+	"errors"
+	"github.com/AnikinSimon/avito-test-backend/internal/httpserver/handler"
+	"github.com/AnikinSimon/avito-test-backend/internal/models/dto/response"
+	"github.com/AnikinSimon/avito-test-backend/internal/models/entity"
+	jwttoken "github.com/AnikinSimon/avito-test-backend/internal/pkg/jwt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	HeaderAuthorization = "Authorization"
+
+	CtxKeyUserType = "User-Type"
+)
+
+type TokenChecker interface {
+	VerifyToken(token string) (map[string]interface{}, error)
+}
+
+type Service struct {
+	tokenSrv TokenChecker
+}
+
+func New(tokenSrv TokenChecker) *Service {
+	return &Service{
+		tokenSrv: tokenSrv,
+	}
+}
+
+func getToken(ctx *gin.Context) (string, error) {
+	bearerToken := ctx.GetHeader(HeaderAuthorization)
+	splitToken := strings.Split(bearerToken, " ")
+	if len(splitToken) != 2 {
+		return "", errors.New("invalid token")
+	}
+
+	tokenStr := splitToken[1]
+
+	return tokenStr, nil
+}
+
+func (s *Service) AuthMiddleware(neededRole ...entity.Role) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token, err := getToken(ctx)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response.Error{
+				Code:      http.StatusUnauthorized,
+				Message:   err.Error(),
+				RequestID: ctx.GetHeader(handler.HeaderRequestID),
+			})
+			return
+		}
+
+		claims, err := s.tokenSrv.VerifyToken(token)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response.Error{
+				Code:      http.StatusUnauthorized,
+				Message:   err.Error(),
+				RequestID: ctx.GetHeader(handler.HeaderRequestID),
+			})
+			return
+		}
+
+		r, ok := claims[jwttoken.JwtClaimRole]
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response.Error{
+				Code:      http.StatusUnauthorized,
+				Message:   "invalid role",
+				RequestID: ctx.GetHeader(handler.HeaderRequestID),
+			})
+		}
+
+		for _, nRole := range neededRole {
+			if r.(string) != string(nRole) {
+				continue
+			}
+
+			ctx.Set(CtxKeyUserType, claims[jwttoken.JwtClaimRole])
+			ctx.Next()
+			return
+		}
+
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, response.Error{
+			Code:      http.StatusUnauthorized,
+			Message:   "invalid role",
+			RequestID: ctx.GetHeader(handler.HeaderRequestID),
+		})
+	}
+}
